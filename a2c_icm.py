9b7a886ff6edc4245fcda8eab1c672171c94005e
Lukasz Burzawa <lburzawa@purdue.edu>
2018-11-22 16:24:58 -0500

    add curiosity code

--- a2c_icm.py
+++ a2c_icm.py
@@ -19,6 +19,7 @@ from random import sample, randint
 from time import time, sleep
 from tqdm import trange
 from model import DoomNet
+from model import ICM
 
 parser = ArgumentParser()
 _ = parser.add_argument
@@ -27,25 +28,25 @@ args = parser.parse_args()
 
 os.environ['CUDA_VISIBLE_DEVICES'] = '1'
 torch.backends.cudnn.benchmark=True
-#torch.backends.cudnn.deteministic=True
 random.seed(0)
 torch.manual_seed(0)
 
 learning_rate = 0.0001
 discount_factor = 0.99
 epochs = 1000
-training_episodes_per_epoch = 160
-testing_episodes_per_epoch = 40
+training_episodes_per_epoch = 80
+testing_episodes_per_epoch = 20
 seq_len = 20
 frame_repeat = 4
 resolution = [42, 42]
 reward_scaling = 1.0
+reward_intrinsic_scaling = 100.0
 value_loss_scaling = 0.5
 entropy_loss_scaling = 0.01
 max_grad = 40.0
 #config_file_path = "../ViZDoom/scenarios/health_gathering_supreme.cfg"
 #config_file_path = "../ViZDoom/scenarios/my_way_home.cfg"
-config_file_path = "./scenarios/my_way_home.cfg"
+config_file_path = "./scenarios/my_way_home_sparse.cfg"
 #config_file_path = "../ViZDoom/scenarios/rocket_basic.cfg"
 #config_file_path = "../ViZDoom/scenarios/basic.cfg"
 #config_file_path = "../ViZDoom/scenarios/deadly_corridor.cfg"
@@ -64,7 +65,6 @@ def initialize_vizdoom(config_file_path):
     #game.set_screen_format(ScreenFormat.GRAY8)
     game.set_screen_resolution(ScreenResolution.RES_640X480)
     game.set_depth_buffer_enabled(False)
-    game.set_seed(0)
     game.init()
     print("Doom initialized.")
     return game
@@ -93,7 +93,7 @@ if __name__ == '__main__':
 
     game = initialize_vizdoom(config_file_path)
     n = game.get_available_buttons_size()
-    actions = [list(a) for a in it.product([0, 1], repeat=n)]
+    #actions = [list(a) for a in it.product([0, 1], repeat=n)]
     actions = [[0, 0, 0], [1, 0, 0], [0, 0, 1], [0, 1, 0]]
 
     if load_model:
@@ -103,10 +103,14 @@ if __name__ == '__main__':
         model.load_state_dict(my_sd)
     else:
         model = DoomNet(len(actions))
+    model_icm = ICM(len(actions))
     model=model.cuda()
+    model_icm = model_icm.cuda()
     #criterion = nn.SmoothL1Loss()
     criterion = nn.MSELoss()
-    optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
+    nll = nn.CrossEntropyLoss()
+    optimizer = torch.optim.Adam(model.parameters(), lr = learning_rate)
+    optimizer_icm = torch.optim.Adam(model_icm.parameters(), lr = learning_rate)
 
     print("Starting the training!")
     time_start = time()
@@ -114,12 +118,15 @@ if __name__ == '__main__':
     for epoch in range(epochs):
         print("\nEpoch %d\n-------" % (epoch + 1))
         train_episodes_finished = 0
-        train_scores = []
-        health_curr = 100.0
+        train_scores = []        
         loss_value_total = 0.0
         loss_policy_total = 0.0
         loss_entropy_total = 0.0    
         loss_total = 0.0
+        loss_inverse_total = 0.0
+        loss_forward_total = 0.0
+        loss_icm_total = 0.0
+        reward_intrinsic_total = 0.0
         steps_total=0
 
         print("Training...")
@@ -127,24 +134,64 @@ if __name__ == '__main__':
         for learning_step in trange(training_episodes_per_epoch, leave=False):
             t = 0
             loss = 0.0
+            loss_icm = 0.0
             hidden = model.init_hidden()
             game.new_episode()
+
+            inp = preprocess(game.get_state())
+            inp1 = inp
+            (policy, value, hidden) = model(inp, hidden)
+            probs = F.softmax(policy,1)
+            a = probs.multinomial(num_samples=1).detach().item()
+            a_icm = a
+            game.make_action(actions[a], frame_repeat)
+            for i in range(len(hidden)):
+                hidden[i] = hidden[i].detach()
+            isterminal = game.is_episode_finished()
+            if isterminal:
+                continue
+            
             while not game.is_episode_finished():
                 probs_list=[]
                 log_probs_list=[]
                 value_list=[]
                 reward_list=[]
                 loss=0.0
+                loss_icm = 0.0
+            
                 for t in range(seq_len):
                     inp = preprocess(game.get_state())
                     (policy, value, hidden) = model(inp, hidden)
                     probs = F.softmax(policy,1)
                     log_probs = F.log_softmax(policy,1)
+                    #m, index = torch.max(policy, 1)
+                    #a = index.item()
                     a = probs.multinomial(num_samples=1).detach().item()
                     probs_list.append(probs[0, a])
                     log_probs_list.append(log_probs[0, a])
                     value_list.append(value)
-                    reward = game.make_action(actions[a], frame_repeat) / reward_scaling                                                                               
+                    reward = game.make_action(actions[a], frame_repeat) / reward_scaling
+            
+                    inp2 = inp
+                    a_out, emb_out, emb = model_icm(inp1, inp2, a_icm)       
+                    a_label = torch.LongTensor([a_icm]).cuda()
+                    reward_intrinsic = (emb_out - emb).pow(2).mean().item() / reward_intrinsic_scaling
+                    reward += reward_intrinsic
+                    reward = min(reward, 1.0)
+                    #print(reward_intrinsic)
+                    loss_inverse = nll(a_out, a_label)
+                    loss_forward = criterion(emb_out, emb)
+                    loss_icm = 0.8 * loss_inverse + 0.2 * loss_forward 
+                    loss_inverse_total += loss_inverse.item()
+                    loss_forward_total += loss_forward.item()
+                    reward_intrinsic_total += reward_intrinsic
+                    inp1 = inp2        
+                    a_icm = a             
+                    optimizer_icm.zero_grad()
+                    loss_icm.backward()
+                    torch.nn.utils.clip_grad_norm_(model_icm.parameters(), max_grad)
+                    optimizer_icm.step()
+                    
                     #print(reward)
                     reward_list.append(reward)           
                     isterminal = game.is_episode_finished()
@@ -172,6 +219,7 @@ if __name__ == '__main__':
                     loss_value_total += loss_value.item()
                     loss_entropy_total += loss_entropy.item()
                 optimizer.zero_grad()
+                loss *= 0.1
                 loss.backward()
                 torch.nn.utils.clip_grad_norm_(model.parameters(), max_grad)
                 optimizer.step()
@@ -185,6 +233,8 @@ if __name__ == '__main__':
         training_steps += steps_total
         print("Results: mean: {:.2f} std: {:.2f}, min: {:.2f}, max: {:.2f}".format(train_scores.mean(), train_scores.std(), train_scores.min(), train_scores.max()))
         print('Loss_policy: {:f}, loss_value: {:f}, loss_entropy: {:f}'.format(loss_policy_total/steps_total, loss_value_total/steps_total, loss_entropy_total/steps_total))
+        print('Reward intrinsic: {:f}, Loss_inverse: {:f}, loss_forward: {:f}'.format(reward_intrinsic_total/steps_total, loss_inverse_total/steps_total, loss_forward_total/steps_total))
+
 
         with torch.no_grad():
             print("\nTesting...")
--- model.py
+++ model.py
@@ -14,26 +14,30 @@ import argparse
 class DoomNet(nn.Module):
     def __init__(self, num_classes):
         super(DoomNet,self).__init__()
-        self.relu = nn.ReLU(inplace=True)
-        self.conv1 = nn.Conv2d(3, 32, kernel_size=8, stride=4, padding=3)
-        self.conv2 = nn.Conv2d(32, 64, kernel_size=4, stride=2, padding=2)
-        self.fc1 = nn.Linear(64*10*13,1024)
-        self.dropout = nn.Dropout(p=0.5)
-        self.lstm1 = nn.LSTMCell(1024,1024)
-        self.fc_val = nn.Linear(1024,1)
-        self.fc = nn.Linear(1024,num_classes)
+        self.relu = nn.ELU(inplace=True)
+        self.conv1 = nn.Conv2d(3, 32, kernel_size=3, stride=2, padding=1)
+        self.conv2 = nn.Conv2d(32, 32, kernel_size=3, stride=2, padding=1)
+        self.conv3 = nn.Conv2d(32, 32, kernel_size=3, stride = 2, padding = 1)
+        self.conv4 = nn.Conv2d(32, 32, kernel_size = 3, stride = 2, padding = 1)
+        #self.fc1 = nn.Linear(32 * 3 * 3, 1024)
+        #self.dropout = nn.Dropout(p=0.5)
+        self.lstm1 = nn.LSTMCell(32 * 3 * 3, 256)
+        self.fc_val = nn.Linear(256, 1)
+        self.fc = nn.Linear(256, num_classes)
 
     def forward(self, x, state):
         hx1i=state[0]
         cx1i=state[1]
 
-        x=self.relu(self.conv1(x))
-        x=self.relu(self.conv2(x))
+        x = self.relu(self.conv1(x))
+        x = self.relu(self.conv2(x))
+        x = self.relu(self.conv3(x))
+        x = self.relu(self.conv4(x))
    
-        x=x.view(1,64*10*13)
-        x=self.dropout(self.relu(self.fc1(x)))
+        x=x.view(1, 32 * 3 * 3)
+        #x=self.dropout(self.relu(self.fc1(x)))
         (hx1o,cx1o)=self.lstm1(x,(hx1i,cx1i))
-        hx1o=self.dropout(hx1o); cx1o=self.dropout(cx1o)
+        #hx1o=self.dropout(hx1o); cx1o=self.dropout(cx1o)
         v=self.fc_val(hx1o)
         y=self.fc(hx1o)
 
@@ -43,8 +47,48 @@ class DoomNet(nn.Module):
 
     def init_hidden(self):
         state=[]
-        state.append(torch.zeros(1,1024).cuda())
-        state.append(torch.zeros(1,1024).cuda())
+        state.append(torch.zeros(1, 256).cuda())
+        state.append(torch.zeros(1, 256).cuda())
         return state
 
+class ICM(nn.Module):
+    def __init__(self, num_classes):
+        super(ICM, self).__init__()
+        self.num_classes = num_classes
+        self.relu = nn.ELU(inplace=True)
+        self.conv1 = nn.Conv2d(3, 32, kernel_size=3, stride=2, padding=1)
+        self.conv2 = nn.Conv2d(32, 32, kernel_size=3, stride=2, padding=1)
+        self.conv3 = nn.Conv2d(32, 32, kernel_size=3, stride = 2, padding = 1)
+        self.conv4 = nn.Conv2d(32, 32, kernel_size = 3, stride = 2, padding = 1)
+        self.inverse_fc1 = nn.Linear(288 * 2, 256)
+        self.inverse_fc2 = nn.Linear(256, num_classes)
+        self.forward_fc1 = nn.Linear(288 + num_classes, 256)
+        self.forward_fc2 = nn.Linear(256, 288)
+
+    def forward(self, x1, x2, a):
+        
+        a_in = torch.zeros(1, self.num_classes).cuda()
+        a_in[0, a] = 1.0
+
+        x1 = self.relu(self.conv1(x1))
+        x1 = self.relu(self.conv2(x1))
+        x1 = self.relu(self.conv3(x1))
+        x1 = self.relu(self.conv4(x1))
+        emb1 = x1.view(1, 32 * 3 * 3)
+
+        x2 = self.relu(self.conv1(x2))
+        x2 = self.relu(self.conv2(x2))
+        x2 = self.relu(self.conv3(x2))
+        x2 = self.relu(self.conv4(x2))
+        emb2 = x2.view(1, 32 * 3 * 3)
+        
+        x = torch.cat((emb1, emb2), 1)
+        x = self.relu(self.inverse_fc1(x))
+        a_out = self.inverse_fc2(x)
+
+        x = torch.cat((emb1.detach(), a_in), 1)
+        x = self.relu(self.forward_fc1(x))
+        emb2_out = self.forward_fc2(x)
+
+        return (a_out, emb2_out, emb2.detach())
 
